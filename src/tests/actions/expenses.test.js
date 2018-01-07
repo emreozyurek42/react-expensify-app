701798b3779d34c6063d9ba6fe34cb6bb515f769
emre <test@example.com>
2018-01-07 00:20:29 -0600

    Add start edit expense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,4 @@
-import { startAddExpense,addExpense, editExpense, removeExpense,
+import { startAddExpense,addExpense, editExpense, removeExpense,startEditExpense,
     setExpenses,starSetExpenses, startRemoveExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import configureMockStore from 'redux-mock-store';
@@ -49,6 +49,25 @@ test('should set up edit expense action object', () => {
             note:'New value'
         }
     });
+});
+
+test('should edit expense from firebase',(done)=> {
+   const store = createMockStore({});
+   const id = expenses[0].id;
+   const updates ={amount:21045};
+   store.dispatch(startEditExpense(id,updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+        type: 'EDIT_EXPENSE',
+        id,
+        updates
+    });
+    return database.ref(`expenses/${id}`).once(value);
+   }).then((snapshot) => {
+      expect(snapshot.val().amount).toBe(updates.amount);
+      done();
+   });
+
 });
 
 test('should setup add expense action object with provided value',() => {
